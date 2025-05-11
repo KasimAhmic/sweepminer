c877ecd9b749590cba9bdf7c62c471864ff21f65
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-05-11 19:07:04 -0400

    Add menu

--- src/include/resource.h
+++ src/include/resource.h
@@ -10,10 +10,22 @@
 #define IDI_SWEEP_MINER         110
 
 // Dialogs
-// #define IDM_                    120
+#define IDM_GAME_NEW            120
+#define IDM_GAME_BEGINNER       121
+#define IDM_GAME_INTERMEDIATE   122
+#define IDM_GAME_EXPERT         123
+#define IDM_GAME_CUSTOM         124
+#define IDM_GAME_MARKS          125
+#define IDM_GAME_COLOR          126
+#define IDM_GAME_SOUND          127
+#define IDM_GAME_BEST_TIMES     128
+#define IDM_GAME_EXIT           129
+#define IDM_HELP_GITHUB         130
+#define IDM_HELP_REPORT_ISSUE   131
+#define IDM_HELP_ABOUT          132
 
 // Strings
-#define IDS_APP_TITLE           130
+#define IDS_APP_TITLE           140
 
 // Controls
 #define IDC_STATIC               -1
